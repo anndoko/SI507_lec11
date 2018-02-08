ef7e8c052d8b9924e2ac07658366eb4013a20e04
Anndo Ko <handcuffanndo@gmail.com>
2018-02-08 14:13:36 -0500

    use the methods/attributes

--- scrapehello.py
+++ scrapehello.py
@@ -1,4 +1,4 @@
-# import Beautiful Soup to parse HTML
+# import the python module: Beautiful Soup to parse HTML
 from bs4 import BeautifulSoup
 
 f = open("hello.html")
@@ -7,15 +7,13 @@ soup = BeautifulSoup(html, "html.parser") # make it a BeautifulSoup object
 
 #print(soup.prettify) # use the prettify() method to get a nicely formatted Unicode string
 
-
-
 # look up elements of the page
 ## searching by tag
 all_list_items = soup.find_all("li")
 all_divs = soup.find_all("div")
 
 ## searching by class
-all_goodbye_elements = soup.find_all(class_="goodbye")
+all_goodbye_elements = soup.find_all(class_="goodbye") # "class" is a researved word in python, so we neeed an underscore here
 
 ## searching by tag AND class
 all_french_list_items = soup.find_all("li", class_="french")
@@ -32,3 +30,31 @@ print("----------")
 print("goodbye elements:\n", all_goodbye_elements)
 print("----------")
 print("french stuff:\n", all_french_list_items)
+
+print(type(all_list_items[0]))
+print("----------")
+
+print("All list items:")
+for li in all_list_items:
+  print(li.string)
+print("----------")
+
+print("Children of hello-list:")
+for child in all_hello_elements[0].children:
+  print(child.string)
+print("----------")
+
+print("List items within the hello tag:")
+hello_list_items = all_hello_elements[0].find_all("li")
+print (hello_list_items)
+print("----------")
+
+print("The hello-list element:")
+the_hello_element = soup.find(id="hello-list")
+print(the_hello_element)
+print("----------")
+
+img_tag = soup.find("img")
+print("The img source:")
+print(img_tag["src"])
+print("----------")
